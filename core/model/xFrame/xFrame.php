a93c3da1db034790067464cf2d9e4692f45cf11d
HitkoDev <development@hitko.si>
2015-04-26 15:55:17 +0200

    query

--- core/model/xFrame/xFrame.php
+++ core/model/xFrame/xFrame.php
@@ -8,7 +8,7 @@ class xFrame {
 		if($_REQUEST['req']) $req = array_map('trim', explode('/', $_REQUEST['req']));
 		foreach($req as $val){
 			$val = array_filter(array_map('trim', explode(':', $val)));
-			if(count($val) > 1) $query[$val[0]] = array_shift($val);
+			if(count($val) > 1) $query[array_shift($val)] = $val;
 		}
 		$this->query = $query;
 	}
